5994945b7222cfa30abf27c254794dae30c6edee
panda2134 <ljypanda@live.com>
2020-02-10 14:43:44 +0800

    remove submodule

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+import routes
+import models
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    routes.init_app(app)
+    models.init_app(app)
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 SECRET = 'smhwN32TtnHE-t3CJgQsZUibu-el8F6ptRHz8ouVemo'
-SQLALCHEMY_DATABASE_API = 'mysql+pymysql://root:Bm24%7Wz%r@localhost/VueBlog'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Bm24%7Wz%r@localhost/VueBlog'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
